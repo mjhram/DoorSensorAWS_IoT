1597af1fd8cc872e7a41851884fc3cd8f2f6438d
tmpram <tmpram@yahoo.com>
2017-08-18 16:38:26 +0300

    -deepSleep added: reportState  every 1h -remove sensor handler -remove comments -add .pem files to github -blink led

--- DoorSensor AWS IoT/fs/init.js
+++ DoorSensor AWS IoT/fs/init.js
@@ -4,84 +4,45 @@ load("api_sys.js");
 load("api_timer.js");
 load('api_esp8266.js');
 load('api_config.js');
-
-// TODO: support other platforms
-let LED_GPIO = 2;
-let LED_OFF = false;
-let LED_ON = true;
-let BUTTON_GPIO = 4;
-
+let LED = 2;
+let SENSOR = 4;
 let state = {
   counter: 0,
-  ledOn: LED_OFF,
   sts: 0//sensor state
 };
-let timerid = -1;
-
-function updateLed() {
-  GPIO.write(LED_GPIO, state.ledOn ? 0 : 1);
-  if(state.ledOn === LED_ON) {
-    //turn off after some time
-    timerid = Timer.set(2*60*1000 , false /* once */, function() {
-      let updRes = AWS.Shadow.update(0, {
-        desired: {
-          counter: state.counter + 1,
-          ledOn: LED_OFF
-        }
-      });
-      timerid = -1;
-      print("Click! Updated:", updRes);
-    }, null);
-  }
-}
-
+GPIO.set_mode(LED, GPIO.MODE_OUTPUT);
+Timer.set(500 , true , function() {
+  GPIO.toggle(LED);
+}, null);
 function updateState(newSt) {
   if (newSt.counter !== undefined) {
     state.counter = newSt.counter;
   }
-
-  if (newSt.ledOn !== undefined) {
-    state.ledOn = newSt.ledOn;
+  if (newSt.sts !== undefined) {
+    state.sts = newSt.sts;
   }
 }
-
 function reportState() {
-  state.sts = GPIO.read(BUTTON_GPIO);
-  print('Reporting state:', JSON.stringify(state));
+  state.sts = GPIO.read(SENSOR);
   AWS.Shadow.update(0, {
     reported: state,
   });
 }
-
-GPIO.set_mode(LED_GPIO, GPIO.MODE_OUTPUT);
-updateLed();
-
 AWS.Shadow.setStateHandler(function(ud, ev, reported, desired, reported_md, desired_md) {
-  print('Event:', ev, '('+AWS.Shadow.eventName(ev)+')');
-
   if (ev === AWS.Shadow.CONNECTED) {
     reportState();
-    Timer.set(1*60*1000 , false /* once */, function() {
+    Timer.set(1*60*1000 , false , function() {
       print('Deep:', "Sleep");
       let deepSleep = ffi('int mgos_system_deep_sleep_d(double)');
       deepSleep(60*60e6);
     }, null);
     return;
   }
-
   if (ev !== AWS.Shadow.GET_ACCEPTED && ev !== AWS.Shadow.UPDATE_DELTA) {
     return;
   }
-
-  print('Reported state:', JSON.stringify(reported));
-  print('Desired state :', JSON.stringify(desired));
-
   updateState(reported);
   updateState(desired);
-  updateLed();
-
-  print('New state:', JSON.stringify(state));
-
   if (ev === AWS.Shadow.UPDATE_DELTA) {
     reportState();
   }
